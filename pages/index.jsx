a2dee410d1232572f1e8097ae2b0f3c19cf6a0cf
Michael Kaufman <mkaufman94@gmail.com>
2021-02-13 13:07:26 -0600

    feat: Implement tutorial through pre-rendering and data fetching (#1)
    * feat: Update starter project
    
    * feat: Add eslint and prettier and convert to jsx files
    
    * feat: Use next link to add links
    
    * feat: Add tailwind config and dependencies
    
    * feat: Add tailwind CSS to global CSS and create layout component
    
    * feat: Furthering tutorial styles
    
    * feat: Populate blog section with posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,40 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Home() {
+import Layout from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
-    <div className="container">
+    <Layout home>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
+        <Image
+          src="/images/profile.jpg"
+          height={144}
+          width={144}
+          alt="Your Name"
+        />
+
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{" "}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className="description">
@@ -54,7 +78,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -68,6 +92,7 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
+
         main {
           padding: 5rem 0;
           flex: 1;
@@ -76,6 +101,7 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
+
         footer {
           width: 100%;
           height: 100px;
@@ -84,40 +110,49 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
+
         footer img {
           margin-left: 0.5rem;
         }
+
         footer a {
           display: flex;
           justify-content: center;
           align-items: center;
         }
+
         a {
           color: inherit;
           text-decoration: none;
         }
+
         .title a {
           color: #0070f3;
           text-decoration: none;
         }
+
         .title a:hover,
         .title a:focus,
         .title a:active {
           text-decoration: underline;
         }
+
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
         }
+
         .title,
         .description {
           text-align: center;
         }
+
         .description {
           line-height: 1.5;
           font-size: 1.5rem;
         }
+
         code {
           background: #fafafa;
           border-radius: 5px;
@@ -126,14 +161,17 @@ export default function Home() {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
+
         .grid {
           display: flex;
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
+
           max-width: 800px;
           margin-top: 3rem;
         }
+
         .card {
           margin: 1rem;
           flex-basis: 45%;
@@ -145,24 +183,29 @@ export default function Home() {
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
+
         .card:hover,
         .card:focus,
         .card:active {
           color: #0070f3;
           border-color: #0070f3;
         }
+
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
+
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
+
         .logo {
           height: 1em;
         }
+
         @media (max-width: 600px) {
           .grid {
             width: 100%;
@@ -180,10 +223,27 @@ export default function Home() {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
+
         * {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
-  )
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section>
+        <h2 className="font-bold text-lg">Blog</h2>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="mb-2" key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
 }
