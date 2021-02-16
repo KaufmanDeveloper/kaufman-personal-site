3243cfa201127c52ca6fa59e91b625b684486cf9
Michael Kaufman <mkaufman94@gmail.com>
2021-02-15 21:22:58 -0600

    feat: Implement interactivity with socials

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import GithubIcon from "./GithubIcon";
 import YouTubeIcon from "./YouTubeIcon";
 import TwitterIcon from "./TwitterIcon";
@@ -6,17 +8,23 @@ export default function Footer() {
   return (
     <div className="relative">
       <div className="fixed w-full bottom-0 my-4 flex items-center">
-        <div>
-          <GithubIcon />
-        </div>
+        <Link href="https://github.com/KaufmanDeveloper">
+          <a>
+            <GithubIcon className="text-primary hover:text-secondary" />
+          </a>
+        </Link>
 
-        <div className="ml-4">
-          <YouTubeIcon />
-        </div>
+        <Link href="https://www.youtube.com/channel/UC8q1rPCdCiU9nzQ07YgCCKQ">
+          <a className="ml-4">
+            <YouTubeIcon className="text-primary hover:text-secondary" />
+          </a>
+        </Link>
 
-        <div className="ml-4">
-          <TwitterIcon />
-        </div>
+        <Link href="https://twitter.com/KaufmanDev">
+          <a className="ml-4">
+            <TwitterIcon className="text-primary hover:text-secondary" />
+          </a>
+        </Link>
       </div>
     </div>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
 import Date from "../components/date";
 
 import Header from "../components/header";
