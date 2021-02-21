b7ab7667fa705232e52f2ce538679c238bc71921
Michael Kaufman <mkaufman94@gmail.com>
2021-02-21 12:26:41 -0600

    feat: Include project example pictures and update home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,18 @@
 import Head from "next/head";
+import Image from "next/image";
 
 import Layout from "../components/layout";
 import Journal from "../components/Journal";
 
-import { Section, SectionHeader, SectionContent } from "../components/Content";
+import { screenSizes } from "../helpers/constants";
+import useMediaQuery from "../helpers/useMediaQuery";
+
+import {
+  Section,
+  SectionHeader,
+  SectionContent,
+  SectionSubHeader,
+} from "../components/Content";
 
 import { getSortedPostsData } from "../lib/posts";
 
@@ -17,21 +26,50 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
+  const isMobileView = useMediaQuery(screenSizes.md);
+
   return (
     <Layout home>
       <Head>
         <title>Michael Kaufman's Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Section>
         <SectionHeader>Nice to meet you!</SectionHeader>
 
         <SectionContent>
           My name is Michael Kaufman. I’m a web developer based out of
-          Nashville, TN and I have a passion for making awesome things!{" "}
+          Nashville, TN and I have a passion for making awesome things!
         </SectionContent>
       </Section>
+
+      <div
+        className={`${
+          !isMobileView ? "flex items-center" : ""
+        } w-full bg-gray-100 rounded mt-8`}
+      >
+        <SectionContent
+          className={`${!isMobileView ? "w-1/2 px-12" : "px-6"} pt-4`}
+        >
+          I love to work and learn with teams that love creating shippable
+          products. I've worked with a government team maintaining legacy
+          software as well as a team using the latest front end development
+          practices to rapidly develop and prototype.
+        </SectionContent>
+
+        <SectionContent
+          className={`${!isMobileView ? "w-1/2 px-10" : "px-6"} my-6`}
+        >
+          <Image
+            className="rounded"
+            src="/images/email-template.png"
+            layout="responsive"
+            width={600}
+            height={300}
+            alt="Michael Kaufman"
+          />
+        </SectionContent>
+      </div>
 
       <Journal allPostsData={allPostsData} />
     </Layout>
