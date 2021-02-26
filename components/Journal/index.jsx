7b19e4df55e52644d5ca94a2527517103ec32f04
Michael Kaufman <mkaufman94@gmail.com>
2021-02-25 20:40:29 -0600

    style: Journal component responsive styling

--- components/Journal/index.jsx
+++ components/Journal/index.jsx
@@ -1,27 +1,29 @@
 import Link from "next/link";
 import Date from "../date";
 
-import { Section, SectionHeader } from "../Content";
+import { Section, SectionContent, SectionHeader } from "../Content";
 
 export default function Journal({ allPostsData }) {
   return (
     <Section className="mt-12 max-w-xl">
       <SectionHeader className="text-4xl text-primary">Journal</SectionHeader>
 
-      <ul className="mt-8">
-        {allPostsData.map(({ id, date, title }) => (
-          <li className="my-4" key={id}>
-            <div className="text-blue-500 hover:text-blue-400">
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-            </div>
-            <div className="text-sm text-gray-700">
-              <Date dateString={date} />
-            </div>
-          </li>
-        ))}
-      </ul>
+      <SectionContent className="mt-4 lg:mt-6">
+        <ul className="mt-8">
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="my-4" key={id}>
+              <div className="text-blue-500 hover:text-blue-400">
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+              </div>
+              <div className="text-sm text-gray-500">
+                <Date dateString={date} />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </SectionContent>
     </Section>
   );
 }
