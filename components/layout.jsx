004572427f6b6c61c3769244badd6f3fce2d9095
Michael Kaufman <mkaufman94@gmail.com>
2021-03-11 18:17:49 -0600

    wip: Working on banner text styling

--- components/layout.jsx
+++ components/layout.jsx
@@ -6,6 +6,8 @@ import { withRouter } from "next/router";
 import Header from "./Header";
 import Footer from "./footer";
 
+import layoutStyles from "./layout.module.css";
+
 export const siteTitle = "Michael Kaufman's Blog";
 
 function Layout({ router, children }) {
@@ -31,6 +33,14 @@ function Layout({ router, children }) {
         {router.pathname === "/" ? (
           <div className="relative pt-half md:pt-most overflow-hidden">
             <Header isHomeVersion />
+
+            <div
+              className={`absolute text-white z-50 top-32 md:top-64 text-center left-0 right-0 leading-10 ${layoutStyles.layout}`}
+            >
+              <div className="text-4xl mx-10">
+                Michael Kaufman is a Web Developer based in Nashville, TN
+              </div>
+            </div>
 
             <Image
               className=""
