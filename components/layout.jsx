fa49011d58d4919804809b1cab9b1dfe0810d5e0
Michael Kaufman <m.kaufman@hahparking.com>
2021-06-23 08:49:52 -0500

    feat: Add shimmer effect to loading image

--- components/layout.jsx
+++ components/layout.jsx
@@ -11,6 +11,25 @@ import layoutStyles from "./layout.module.css";
 export const siteTitle = "Michael Kaufman's Blog";
 
 function Layout({ router, children }) {
+  const shimmer = (w, h) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
+
+  const toBase64 = (str) =>
+    typeof window === "undefined"
+      ? Buffer.from(str).toString("base64")
+      : window.btoa(str);
+
   return (
     <div>
       <div className="min-h-90">
@@ -51,6 +70,10 @@ function Layout({ router, children }) {
               alt="Michael Kaufman"
               layout="fill"
               objectFit="cover"
+              placeholder="blur"
+              blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                shimmer(700, 475)
+              )}`}
             />
           </div>
         ) : (
