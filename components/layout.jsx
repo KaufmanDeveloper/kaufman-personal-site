c2cba63fa1c6dfa56edec5eb76663b7329a20a0c
Michael Kaufman <mkaufman94@gmail.com>
2021-06-23 08:55:57 -0500

    feat: Add placeholder shimmer effect for landing banner (#13)
    * feat: Add shimmer effect to loading image
    
    * feat: Add shimmer loading placeholder
    
    Co-authored-by: Michael Kaufman <m.kaufman@hahparking.com>

--- components/layout.jsx
+++ components/layout.jsx
@@ -6,11 +6,28 @@ import { withRouter } from "next/router";
 import Header from "./Header";
 import Footer from "./footer";
 
-import layoutStyles from "./layout.module.css";
-
 export const siteTitle = "Michael Kaufman's Blog";
 
 function Layout({ router, children }) {
+  const shimmer = (w, h) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
+
+  const toBase64 = (str) =>
+    typeof window === "undefined"
+      ? Buffer.from(str).toString("base64")
+      : window.btoa(str);
+
   return (
     <div>
       <div className="min-h-90">
@@ -35,10 +52,10 @@ function Layout({ router, children }) {
             <Header isHomeVersion />
 
             <div
-              className={`absolute z-50 py-4 top-1/2 md:top-80 text-center left-0 right-0 leading-10 ${layoutStyles.layout} flex justify-center`}
+              className={`absolute z-50 py-4 top-1/2 md:top-80 text-center left-0 right-0 leading-10 flex justify-center`}
             >
               <div
-                className={`text-xl md:text-4xl leading-8 md:leading-12 text-white mx-10 w-10/12 md:w-8/12 text-center ${layoutStyles.text}`}
+                className={`text-xl md:text-4xl leading-8 md:leading-12 text-white mx-10 w-10/12 md:w-8/12 text-center`}
                 aria-multiline="true"
               >
                 Michael Kaufman is a Web Developer based in Nashville, TN
@@ -46,11 +63,14 @@ function Layout({ router, children }) {
             </div>
 
             <Image
-              className={layoutStyles.image}
               src="/images/at-desk.jpeg"
               alt="Michael Kaufman"
               layout="fill"
               objectFit="cover"
+              placeholder="blur"
+              blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                shimmer(700, 475)
+              )}`}
             />
           </div>
         ) : (
