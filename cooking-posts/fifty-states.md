1c20a3d339ff7e18ec10e0110f7bb5c075e9975a
Michael Kaufman <mkaufman94@gmail.com>
2022-11-30 16:22:21 -0600

    feat: Include cooking-posts directory

---
+++ cooking-posts/fifty-states.md
@@ -0,0 +1,6 @@
+---
+title: 'Recipes Across the US'
+date: '2022-11-29'
+---
+
+Recipes.
---
+++ pages/cooking-posts/[id].js
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+
+import {
+  Section,
+  SectionContent,
+  SectionHeader,
+} from '../../components/Content'
+import Layout from '../../components/layout'
+import Date from '../../components/date'
+
+import { getAllPostIds, getPostData } from '../../lib/cooking-posts'
+
+import postStyles from './posts.module.css'
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData,
+    },
+  }
+}
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      <article className={postStyles.posts}>
+        <Section>
+          <SectionHeader>{postData.title}</SectionHeader>
+
+          <div className="mt-4 text-gray-500">
+            <Date dateString={postData.date} />
+          </div>
+
+          <SectionContent className="posts w-full lg:w-8/12">
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          </SectionContent>
+        </Section>
+      </article>
+    </Layout>
+  )
+}
