af4b1898a1891c528a905175bea29fe462f04c00
Michael Kaufman <mkaufman94@gmail.com>
2023-12-13 15:44:15 -0600

    feat: Set placeholder for cover art as gray background (#30)

--- components/layout.jsx
+++ components/layout.jsx
@@ -14,6 +14,20 @@ export const siteTitle = "Michael Kaufman's Blog"
 function Layout({ router, children }) {
   const isMobileView = useMediaQuery(screenSizes.sm)
 
+  const keyStr =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+
+  const triplet = (e1, e2, e3) =>
+    keyStr.charAt(e1 >> 2) +
+    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+    keyStr.charAt(e3 & 63)
+
+  const rgbDataURL = (r, g, b) =>
+    `data:image/gif;base64,R0lGODlhAQABAPAA${
+      triplet(0, r, g) + triplet(b, 255, 255)
+    }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
+
   return (
     <div>
       <div className="min-h-90">
@@ -67,6 +81,8 @@ function Layout({ router, children }) {
               layout="fill"
               objectFit="cover"
               priority={true}
+              placeholder="blur"
+              blurDataURL={rgbDataURL(183, 183, 183)}
             />
           </div>
         ) : (
