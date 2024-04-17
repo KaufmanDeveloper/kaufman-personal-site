3bcbda012d2add2fc77842ca11874baf52738adf
mkaufman94@gmail.com <mkaufman94@gmail.com>
2024-04-17 08:29:29 -0500

    style: Add margins

--- components/PortfolioElement/index.jsx
+++ components/PortfolioElement/index.jsx
@@ -13,23 +13,25 @@ export default function PortfolioElement({
 
   return (
     <div className={isMobileView ? 'w-full' : 'w-1/2'}>
-      <Link href={externalLink} passHref>
-        <a className="hover:opacity-90">
-          <div className="relative pt-half md:pt-half overflow-hidden flex justify-center">
-            <div
-              className={`absolute z-50 py-4 top-1/3 md:top-80 text-center left-0 right-0 leading-10 flex justify-center`}
-            >
+      <div className={`${!isMobileView && 'mx-2'} my-2`}>
+        <Link href={externalLink} passHref>
+          <a className="hover:opacity-90">
+            <div className="relative pt-half md:pt-half overflow-hidden flex justify-center">
               <div
-                className={`text-xl md:text-4xl leading-8 md:leading-12 text-white mx-10 w-10/12 md:w-8/12 text-center`}
-                aria-multiline="true"
+                className={`absolute z-50 py-4 top-1/3 md:top-80 text-center left-0 right-0 leading-10 flex justify-center`}
               >
-                {projectName}
+                <div
+                  className={`text-xl md:text-4xl leading-8 md:leading-12 text-white mx-10 w-10/12 md:w-8/12 text-center`}
+                  aria-multiline="true"
+                >
+                  {projectName}
+                </div>
               </div>
+              <HeaderImage imageLink={imageLink} />
             </div>
-            <HeaderImage imageLink={imageLink} />
-          </div>
-        </a>
-      </Link>
+          </a>
+        </Link>
+      </div>
     </div>
   )
 }
